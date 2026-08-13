bb00fda343c5489096f5a37e18c774f5b986de47
charangowda20 <charangowdak255@gmail.com>
2026-08-13 14:14:17 +0530

    Add SubtractNumbers class to calculate difference

---
+++ Subtract.java
@@ -0,0 +1,10 @@
+public class SubtractNumbers {
+    public static void main(String[] args) {
+        int num1 = 20;
+        int num2 = 10;
+
+        int difference = num1 - num2;
+
+        System.out.println("Difference = " + difference);
+    }
+}
