fa22ff3234f605bb6dc3cb4970945ed8f2ca484e
charangowda20 <charangowdak255@gmail.com>
2026-08-13 14:18:25 +0530

    Add AverageNumbers class to calculate average

---
+++ Average.java
@@ -0,0 +1,10 @@
+public class AverageNumbers {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+
+        double average = (num1 + num2) / 2.0;
+
+        System.out.println("Average = " + average);
+    }
+}
