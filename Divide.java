734fdff6b0b40dcb799ef57be5786ead6142ba57
charangowda20 <charangowdak255@gmail.com>
2026-08-13 14:17:32 +0530

    Add DivideNumbers class for division operation

---
+++ Divide.java
@@ -0,0 +1,10 @@
+public class DivideNumbers {
+    public static void main(String[] args) {
+        int num1 = 20;
+        int num2 = 10;
+
+        int result = num1 / num2;
+
+        System.out.println("Result = " + result);
+    }
+}
